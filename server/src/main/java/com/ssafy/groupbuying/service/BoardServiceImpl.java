6f65e75dbe7ce32cf2b9e831125e2f64d2e406ad
juyeon <wjg365@naver.com>
2020-06-02 15:45:13 +0900

    setting DB

--- server/src/main/java/com/ssafy/groupbuying/service/BoardServiceImpl.java
+++ server/src/main/java/com/ssafy/groupbuying/service/BoardServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.groupbuying.repository.BoardRepository;
+import com.ssafy.groupbuying.repository.CommentRepository;
 import com.ssafy.groupbuying.repository.ParticipantsRepository;
 import com.ssafy.groupbuying.repository.ReputationRepository;
 import com.ssafy.groupbuying.repository.UserRepository;
@@ -36,6 +37,8 @@ public class BoardServiceImpl implements BoardService {
 	@Override
 	public boolean insert(Board board) {
 		// 게시판을 작성하고
+		System.out.println("INSERT!!!");
+		System.out.println(board.toString());
 		repo.save(board);
 		return true;
 	}
