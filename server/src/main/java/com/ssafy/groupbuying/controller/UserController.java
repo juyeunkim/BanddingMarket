82b40c2c3f9ee79e76129940a1813d79acdab107
juyeon <wjg365@naver.com>
2020-06-04 17:53:31 +0900

    add user CORS

--- server/src/main/java/com/ssafy/groupbuying/controller/UserController.java
+++ server/src/main/java/com/ssafy/groupbuying/controller/UserController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
 @RequestMapping("user")
 public class UserController {
