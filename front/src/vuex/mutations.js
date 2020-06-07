50d42a05e39535ba8d805cd725f98020d2d9d0fe
김태민 <taeminkim23@gmail.com>
2020-06-07 10:28:59 +0900

    complete  searchALL

--- front/src/vuex/mutations.js
+++ front/src/vuex/mutations.js
@@ -6,8 +6,15 @@ export default {
         store.state.test = payload.test;
     },
     [Constant.SET_SEARCHLIST]: (state, payload) => {
-        store.state.searchList = store.state.searchList.concat(payload.searchList);
+        // store.state.searchList = store.state.searchList.concat(payload.searchList);
+        store.state.searchList = payload.searchList
         console.log(store.state.searchList)
     },
+    [Constant.SET_BOARD]: (state, payload) => {
+        // store.state.searchList = store.state.searchList.concat(payload.searchList);
+        store.state.board = payload.board
+        console.log(store.state.board)
+    },
+
 
 };
