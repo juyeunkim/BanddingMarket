ced0073b9fe6b6b771069f91c25b0983f8b5f46e
juyeon <wjg365@naver.com>
2020-06-08 22:16:10 +0900

    fix error

---
+++ front/src/firebase/init.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+// import firestore from 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAzJeSpwuQMUtrh-IXBhy1QzoiEfvgN0fc",
+    authDomain: "bandding-chat.firebaseapp.com",
+    databaseURL: "https://bandding-chat.firebaseio.com",
+    projectId: "bandding-chat",
+    storageBucket: "bandding-chat.appspot.com",
+    messagingSenderId: "295439997275",
+    appId: "1:295439997275:web:1e6b226ff20e9e86f9e27e",
+    measurementId: "G-Z2XST6ZCZR"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// firebaseApp.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebaseApp.firestore();
